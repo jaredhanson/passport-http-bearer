871cc463739fbd14ec8c799cfacde7ab32b2aad4
Jared Hanson <jaredhanson@gmail.com>
2011-10-30 09:30:46 -0700

    Additional test cases for Strategy.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -121,6 +121,78 @@ vows.describe('BearerStrategy').addBatch({
     },
   },
   
+  'strategy handling a request that is not validated': {
+    topic: function() {
+      var strategy = new BearerStrategy(function(token, done) {
+        done(null, false);
+      });
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.fail = function(challenge) {
+          self.callback(null, challenge);
+        }
+        
+        req.headers = {};
+        req.headers.authorization = 'Bearer vF9dft4qmT';
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should fail authentication with challenge' : function(err, challenge) {
+        // fail action was called, resulting in test callback
+        assert.isNull(err);
+        assert.equal(challenge, 'Bearer realm="Users"');
+      },
+    },
+  },
+  
+  'strategy handling a request that encounters an error during verification': {
+    topic: function() {
+      var strategy = new BearerStrategy(function(token, done) {
+        done(new Error('something went wrong'));
+      });
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.fail = function(challenge) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.error = function(err) {
+          self.callback(null, err);
+        }
+        
+        req.headers = {};
+        req.headers.authorization = 'Bearer vF9dft4qmT';
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not call success or fail' : function(err, e) {
+        assert.isNull(err);
+      },
+      'should call error' : function(err, e) {
+        assert.instanceOf(e, Error);
+      },
+    },
+  },
+  
   'strategy handling a request without authorization credentials': {
     topic: function() {
       var strategy = new BearerStrategy(function(token, done) {
@@ -149,6 +221,109 @@ vows.describe('BearerStrategy').addBatch({
         // fail action was called, resulting in test callback
         assert.isNull(err);
         assert.equal(challenge, 'Bearer realm="Users"');
+      },
+    },
+  },
+  
+  'strategy handling a request with non-Bearer authorization header': {
+    topic: function() {
+      var strategy = new BearerStrategy(function(token, done) {
+        done(null, { token: token });
+      });
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.fail = function(challenge) {
+          self.callback(null, challenge);
+        }
+        
+        req.headers = {};
+        req.headers.authorization = 'XXXXX vF9dft4qmT';
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should fail authentication with challenge' : function(err, challenge) {
+        // fail action was called, resulting in test callback
+        assert.isNull(err);
+        assert.equal(challenge, 'Bearer realm="Users"');
+      },
+    },
+  },
+  
+  'strategy handling a request with malformed authorization header': {
+    topic: function() {
+      var strategy = new BearerStrategy(function(token, done) {
+        done(null, { token: token });
+      });
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.fail = function(challenge) {
+          self.callback(null, challenge);
+        }
+        
+        req.headers = {};
+        req.headers.authorization = 'Bearer';
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should fail authentication with challenge' : function(err, challenge) {
+        // fail action was called, resulting in test callback
+        assert.isNull(err);
+        assert.equal(challenge, 'Bearer realm="Users"');
+      },
+    },
+  },
+  
+  'strategy handling a valid request with BEARER scheme in capitalized letters': {
+    topic: function() {
+      var strategy = new BearerStrategy(function(token, done) {
+        done(null, { token: token });
+      });
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(null, user);
+        }
+        strategy.fail = function() {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.headers = {};
+        req.headers.authorization = 'BEARER vF9dft4qmT';
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user) {
+        assert.equal(user.token, 'vF9dft4qmT');
       },
     },
   },
