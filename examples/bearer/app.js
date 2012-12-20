43ccaa7b4da09bb46ca7a200af09f24d18ee20f0
Shao Chen <shaochen@Shaos-MacBook-Air.local>
2012-12-20 15:16:27 -0500

    modified to accept passReqToCallback in options to return request

--- examples/bearer/app.js
+++ examples/bearer/app.js
@@ -43,6 +43,33 @@ passport.use(new BearerStrategy({
   }
 ));
 
+/* 
+// Return back the request object example by passing in options "passReqToCallback": true
+// Use the BearerStrategy within Passport.
+//   Strategies in Passport require a `validate` function, which accept
+//   credentials (in this case, a token), and invoke a callback with a user
+//   object.
+passport.use(new BearerStrategy({ "passReqToCallback": true },
+  function(req, token, done) {
+  	
+  	//req is passed back here
+  	console.log(req);
+    // asynchronous validation, for effect...
+    process.nextTick(function () {
+      
+      // Find the user by token.  If there is no user with the given token, set
+      // the user to `false` to indicate failure.  Otherwise, return the
+      // authenticated `user`.  Note that in a production-ready application, one
+      // would want to validate the token for authenticity.
+      findByToken(token, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        return done(null, user);
+      })
+    });
+  }
+));
+*/
 
 
 
