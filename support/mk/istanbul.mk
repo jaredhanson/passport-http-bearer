6c3316de8e5bd1681aa5d90aae969da3d45a5cef
Jared Hanson <jaredhanson@gmail.com>
2013-08-01 14:22:15 -0700

    Update vows to 0.7.0 and add support files.

---
+++ support/mk/istanbul.mk
@@ -0,0 +1,20 @@
+ISTANBUL ?= istanbul
+ISTANBUL_OUT ?= ./reports/coverage
+ISTANBUL_REPORT ?= lcov
+ISTANBUL_HTML_REPORT_PATH ?= $(ISTANBUL_OUT)/lcov-report/index.html
+ISTANBUL_LCOV_INFO_PATH ?= $(ISTANBUL_OUT)/lcov.info
+
+
+test-istanbul-mocha: node_modules
+	NODE_PATH=$(NODE_PATH_TEST) \
+	$(ISTANBUL) cover \
+	--dir $(ISTANBUL_OUT) --report $(ISTANBUL_REPORT) \
+	$(_MOCHA) -- \
+		--reporter $(MOCHA_REPORTER) \
+		--require $(MOCHA_REQUIRE) $(TESTS)
+
+view-istanbul-report:
+	open $(ISTANBUL_HTML_REPORT_PATH)
+
+
+.PHONY: test-istanbul-mocha view-istanbul-report
