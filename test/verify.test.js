64a526770422b1cc9e93d87491f1e04712f60c9c
Jared Hanson <jaredhanson@gmail.com>
2021-10-26 07:18:50 -0700

    Clarify tests.

--- test/verify.test.js
+++ test/verify.test.js
@@ -63,29 +63,25 @@ describe('verify function', function() {
         })
         .authenticate();
     }); // should authenticate request with case-insensitive scheme
-  
-    describe('that accepts request argument', function() {
-      
-      it('should authenticate request', function(done) {
-        var strategy = new Strategy({ passReqToCallback: true }, function(req, token, cb) {
-          expect(req.url).to.equal('/');
-          expect(token).to.equal('mF_9.B5f-4.1JqM');
-          return cb(null, { id: '248289761001' }, { scope: [ 'profile', 'email' ] });
-        });
-        
-        chai.passport.use(strategy)
-          .request(function(req) {
-            req.headers['authorization'] = 'Bearer mF_9.B5f-4.1JqM';
-          })
-          .success(function(user, info) {
-            expect(user).to.deep.equal({ id: '248289761001' });
-            expect(info).to.deep.equal({ scope: [ 'profile', 'email' ] });
-            done();
-          })
-          .authenticate();
+    
+    it('should accept request argument and authenticate request', function(done) {
+      var strategy = new Strategy({ passReqToCallback: true }, function(req, token, cb) {
+        expect(req.url).to.equal('/');
+        expect(token).to.equal('mF_9.B5f-4.1JqM');
+        return cb(null, { id: '248289761001' }, { scope: [ 'profile', 'email' ] });
       });
       
-    }); // that accepts request argument
+      chai.passport.use(strategy)
+        .request(function(req) {
+          req.headers['authorization'] = 'Bearer mF_9.B5f-4.1JqM';
+        })
+        .success(function(user, info) {
+          expect(user).to.deep.equal({ id: '248289761001' });
+          expect(info).to.deep.equal({ scope: [ 'profile', 'email' ] });
+          done();
+        })
+        .authenticate();
+    }); // should accept request argument and authenticate request
   
   }); // that authenticates
   
@@ -108,47 +104,39 @@ describe('verify function', function() {
         .authenticate();
     }); // should challenge request
     
-    describe('with explanation', function() {
-      
-      it('should challenge request', function(done) {
-        var strategy = new Strategy(function(token, cb) {
-          return cb(null, false, { message: 'The access token expired' });
-        });
-        
-        chai.passport.use(strategy)
-          .request(function(req) {
-            req.headers['authorization'] = 'Bearer mF_9.B5f-4.1JqM';
-          })
-          .fail(function(challenge, status) {
-            expect(challenge).to.equal('Bearer realm="Users", error="invalid_token", error_description="The access token expired"');
-            expect(status).to.be.undefined;
-            done();
-          })
-          .authenticate();
+    it('should challenge request with explanation', function(done) {
+      var strategy = new Strategy(function(token, cb) {
+        return cb(null, false, { message: 'The access token expired' });
       });
       
-    }); // with explanation
+      chai.passport.use(strategy)
+        .request(function(req) {
+          req.headers['authorization'] = 'Bearer mF_9.B5f-4.1JqM';
+        })
+        .fail(function(challenge, status) {
+          expect(challenge).to.equal('Bearer realm="Users", error="invalid_token", error_description="The access token expired"');
+          expect(status).to.be.undefined;
+          done();
+        })
+        .authenticate();
+    }); // should challenge request with explanation
     
-    describe('with explanation as string', function() {
-      
-      it('should challenge request', function(done) {
-        var strategy = new Strategy(function(token, cb) {
-          return cb(null, false, 'The access token expired');
-        });
-        
-        chai.passport.use(strategy)
-          .request(function(req) {
-            req.headers['authorization'] = 'Bearer mF_9.B5f-4.1JqM';
-          })
-          .fail(function(challenge, status) {
-            expect(challenge).to.equal('Bearer realm="Users", error="invalid_token", error_description="The access token expired"');
-            expect(status).to.be.undefined;
-            done();
-          })
-          .authenticate();
+    it('should challenge request with explanation as string', function(done) {
+      var strategy = new Strategy(function(token, cb) {
+        return cb(null, false, 'The access token expired');
       });
       
-    }); // with explanation as string
+      chai.passport.use(strategy)
+        .request(function(req) {
+          req.headers['authorization'] = 'Bearer mF_9.B5f-4.1JqM';
+        })
+        .fail(function(challenge, status) {
+          expect(challenge).to.equal('Bearer realm="Users", error="invalid_token", error_description="The access token expired"');
+          expect(status).to.be.undefined;
+          done();
+        })
+        .authenticate();
+    }); // should challenge request with explanation as string
     
   }); // that does not authenticate
   
